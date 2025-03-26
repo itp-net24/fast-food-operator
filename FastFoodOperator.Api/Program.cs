17d51854b27fa1cb92b9ad62cce40b981661c01a
grahsp <pontus.grahsp@gmail.com>
2025-03-26 21:24:23 +0100

    chore(solution): Initialize new solution and project - Create new solution - Create new aspnet api project - Install and setup swagger for api documentation - Configure project to disallow null references

---
+++ FastFoodOperator.Api/Program.cs
@@ -0,0 +1,31 @@
+namespace FastFoodOperator.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Add services to the container.
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+
+            // Configure the HTTP request pipeline.
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.Run();
+        }
+    }
+}
