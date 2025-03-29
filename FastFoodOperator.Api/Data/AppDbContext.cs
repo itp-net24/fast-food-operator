ecbd180b5d8f833624b0007a098888288004ee49
Pontus Grähs <pontus.grahsp@gmail.com>
2025-03-29 13:19:54 +0100

    chore: Initialize new solution and project (#1)
    * chore(solution): Initialize new solution and project
    - Create new solution
    - Create new aspnet api project
    - Install and setup swagger for api documentation
    - Configure project to disallow null references
    -configure project to treat warnings as errors
    
    * chore(config): register user secrets and add a user secret id to project file
    
    * chore(dependencies): install packages required to setup a database
    
    * feat(context): initialize an empty db context
    
    * feat(database): register context to container
    
    * chore(migration): create initial migration
    
    * fix(project): fix nullable references not being treated as errors

---
+++ FastFoodOperator.Api/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FastFoodOperator.Api.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options)
+        : DbContext(options)
+    {
+    }
+}
---
+++ FastFoodOperator.Api/Program.cs
@@ -0,0 +1,47 @@
+using FastFoodOperator.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FastFoodOperator.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Add services to the container.
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Development
+            if (builder.Environment.IsDevelopment())
+            {
+                builder.Configuration.AddUserSecrets<Program>();
+            }
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+
+
+
+            // Configure middlewares
+            var app = builder.Build();
+
+            // Development
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.Run();
+        }
+    }
+}
