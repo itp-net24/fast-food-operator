9e4480ee382378487419267b83695de4b20ce546
grahsp <pontus.grahsp@gmail.com>
2025-03-31 10:22:22 +0200

    feat(entity): create new entity for a products different variants

---
+++ FastFoodOperator.Api/Data/Models/ProductVariant.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FastFoodOperator.Api.Data
+{
+    public partial class AppDbContext
+    {
+        public class ProductVariant
+        {
+            [Key]
+            public int Id { get; set; }
+
+            public int ProductId { get; set; }
+
+
+            [ForeignKey(nameof(ProductId))]
+            public Product Product { get; set; } = null!;
+
+
+            [Required]
+            [MaxLength(100)]
+            public string Name { get; set; } = string.Empty;
+
+            public string? Description { get; set; }
+
+            [Column(TypeName = "decimal(10, 2)")]
+            public decimal PriceModifier { get; set; }
+        }
+	}   
+}
