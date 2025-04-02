cdd83fbcf4091bfe0e48b8a6dfaf28bc0db4e606
grahsp <pontus.grahsp@gmail.com>
2025-04-02 22:11:35 +0200

    feat(product): add method to create new combo in ProductService

--- FastFoodOperator.Api/Services/ProductService.cs
+++ FastFoodOperator.Api/Services/ProductService.cs
@@ -1,9 +1,46 @@
 using FastFoodOperator.Api.Data;
+using FastFoodOperator.Api.DTOs;
+using FastFoodOperator.Api.Entities;
 
 namespace FastFoodOperator.Api.Services;
 
 public class ProductService(AppDbContext context, ILogger<ProductService> logger)
 {
-	private readonly AppDbContext _context = context;
-	private readonly ILogger<ProductService> _logger = logger;
+	public async Task CreateNewComboAsync(ComboCreateDto dto)
+	{
+		logger.LogInformation("Creating a new combo: {ComboName}", dto.Name);
+		
+		await using var transaction = await context.Database.BeginTransactionAsync();
+		try
+		{
+			var combo = new Combo
+			{
+				Name = dto.Name,
+				BasePrice = dto.BasePrice
+			};
+
+			context.Combos.Add(combo);
+
+			var products = dto.Products.Select(p => new ComboProduct
+			{
+				ComboId = combo.Id,
+				ProductId = p.ProductId,
+				ProductVariantId = p.DefaultVariantId
+			}).ToArray();
+
+			context.ComboProducts.AddRange(products);
+
+			await context.SaveChangesAsync();
+			await transaction.CommitAsync();
+			
+			logger.LogInformation("Successfully created combo: {ComboId}", combo.Id);
+		}
+		catch (Exception ex)
+		{
+			logger.LogError(ex, "Failed to create combo: {ComboName}", dto.Name);	
+			
+			await transaction.RollbackAsync();
+			throw;
+		}
+	}
 }
