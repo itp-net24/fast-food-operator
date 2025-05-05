674b2da350e2b0a79ee24487bbcf48fb1975ab9e
Gabriel-V-Dot <gabriel.viinikka@gmail.com>
2025-05-05 22:11:58 +0200

    Refactored addToCart

--- FastFoodOperator.Client/src/stores/cart.ts
+++ FastFoodOperator.Client/src/stores/cart.ts
@@ -9,6 +9,7 @@ export const useCartStore = defineStore('cart', {
   state: (): State => ({
     cart: { cartProducts: [], cartCombos: [] }
   }),
+
   actions: {
     loadCartInstance() {
       const cs = localStorage.getItem('cart')
@@ -42,27 +43,9 @@ export const useCartStore = defineStore('cart', {
                 products: pro.products
               }],
             cartCombos: []
-
-            // export interface CartContainer {
-            //   id: number;
-            //   type: string;
-            //   imageUrl: string | null;
-            //   name: string;
-            //   price: number;
-            //   quantity: number;
-            //   products: CartItem[];
-            // }
-
-            // export interface CartItem {
-            //   __uid: number;
-            //   id: number;
-            //   name: string;
-            //   variant?: Variant | null;
-            //   ingredients?: Ingredient[] | null;
-            // }
-
           }
         }
+
         else if (pro.type === 'combo') {
           this.cart = {
             cartProducts: [],
@@ -78,11 +61,12 @@ export const useCartStore = defineStore('cart', {
               }]
           }
         }
-        else{
+        else {
           console.error('this type is not supported' + pro.type)
         }
+
       }
-      
+
       //logic for when cart is not empty
       else {
         let cartLocalStorage = JSON.parse(cs)
@@ -91,44 +75,101 @@ export const useCartStore = defineStore('cart', {
         console.log('type:' + pro.type)
         //determine cartcontainer type (combo or product)
         if (pro.type === 'product') {
+          // adding product to cart
+          cartLocalStorage.cartProducts = cartLocalStorage.cartProducts.map((cpi: CartContainer) => {
+            if (cpi.id === pro.id) {
+              isAdded = true;
+              return {
+                id: pro.id,
+                type: pro.type,
+                imageUrl: pro.imageUrl,
+                name: pro.name,
+                price: pro.price,
+                quantity: cpi.quantity + pro.quantity,
+                products: pro.products
+              }
+            }
+            return {
+              id: cpi.id,
+              type: cpi.type,
+              imageUrl: cpi.imageUrl,
+              name: cpi.name,
+              price: cpi.price,
+              quantity: cpi.quantity,
+              products: cpi.products
+            }
+          })
+
+          if (!isAdded) {
+            cartLocalStorage.cartProducts.push({
+              id: pro.id,
+              type: pro.type,
+              imageUrl: pro.imageUrl,
+              name: pro.name,
+              price: pro.price,
+              quantity: pro.quantity,
+              products: pro.products
+            })
+          }
+
+          this.cart = cartLocalStorage;
 
         }
+
         else if (pro.type === 'combo') {
-
+          // adding existing combo to cart
+          cartLocalStorage.cartCombos = cartLocalStorage.cartCombos.map((cci: CartContainer) => {
+            if (cci.id === pro.id) {
+              isAdded = true;
+              return {
+                id: pro.id,
+                type: pro.type,
+                imageUrl: pro.imageUrl,
+                name: pro.name,
+                price: pro.price,
+                quantity: cci.quantity + pro.quantity,
+                products: pro.products
+              }
+            }
+            return {
+              id: cci.id,
+              type: cci.type,
+              imageUrl: cci.imageUrl,
+              name: cci.name,
+              price: cci.price,
+              quantity: cci.quantity,
+              products: cci.products
+            }
+          })
+          //adding new combo that is not already in cart
+          if (!isAdded) {
+            cartLocalStorage.cartCombos.push({
+              id: pro.id,
+              type: pro.type,
+              imageUrl: pro.imageUrl,
+              name: pro.name,
+              price: pro.price,
+              quantity: pro.quantity,
+              products: pro.products
+            })
+          }
         }
 
+        this.cart = cartLocalStorage;
 
         console.log(localStorage.getItem('cart'))
 
-
-
-        cartLocalStorage.cartProducts = cartLocalStorage.cartProducts.map((ci: cartProduct) => {
-          if (ci.product.id === pro.id) {
-            isAdded = true
-            return { product: pro, qty: ci.qty + 1 }
-          }
-          return { product: ci.product, qty: ci.qty }
-        })
-
-        if (!isAdded) {
-          cartLocalStorage.cartProducts.push({ product: pro, qty: 1 })
-        }
-
-        this.cart = cartLocalStorage
+        localStorage.setItem('cart', JSON.stringify(this.cart))
 
       }
-
-      localStorage.setItem('cart', JSON.stringify(this.cart))
-
-
     },
 
     decrementFromCart(pro: Product) {
       console.log('start of decrementFromCart')
       const cs = localStorage.getItem('cart')
-
+    
       let remove = false
-
+    
       console.log(!cs)
       if (!cs) {
         this.cart = {
@@ -137,9 +178,9 @@ export const useCartStore = defineStore('cart', {
         }
       }
       else {
-
+    
         console.log(localStorage.getItem('cart'))
-
+    
         let cartLocalStorage = JSON.parse(cs)
         console.log(cartLocalStorage)
         cartLocalStorage.cartProducts = cartLocalStorage.cartProducts.map((ci: cartProduct) => {
@@ -157,14 +198,14 @@ export const useCartStore = defineStore('cart', {
         if (remove) {
           cartLocalStorage.cartProducts = cartLocalStorage.cartProducts.filter((ci: cartProduct) => ci.product.id != pro.id)
         }
-
+    
         this.cart = cartLocalStorage
-
+    
       }
-
+    
       localStorage.setItem('cart', JSON.stringify(this.cart))
-
-
+    
+    
     },
 
     removeFromCart(pro: Product) {
@@ -172,16 +213,14 @@ export const useCartStore = defineStore('cart', {
       localStorage.setItem('cart', JSON.stringify(this.cart))
     },
 
-    displayCartLoad() { },
-
     clearCart() {
-      (this.cart as Cart) = { cartProducts: [] }
+      (this.cart as Cart) = { cartProducts: [], cartCombos: [] }
       localStorage.setItem('cart', JSON.stringify(this.cart))
     },
 
     checkOut(comment: string = '') {
       console.log('in the checkout action' + comment)
-
+    
       let order: AddOrderDTO = {
         customerNote: comment,
         orderComboDtos: [
@@ -216,9 +255,9 @@ export const useCartStore = defineStore('cart', {
         ]
       }
       console.log(order.orderProductDtos)
-
+    
       console.log(JSON.stringify(order))
-
+    
       //set up array for orderProductDtos
       let orderProductDtosArray = []
       //map all products in cart to orderProductDtos
@@ -229,7 +268,7 @@ export const useCartStore = defineStore('cart', {
         }
       })
       //order.orderProductDtos.push(orderProductDtosArray)
-
+    
       //verify that orderProductDtos were mapped correctly
       console.log(order.orderProductDtos)
       console.log(JSON.stringify(order))
@@ -242,36 +281,35 @@ export const useCartStore = defineStore('cart', {
       //       }
       // })
       // console.log('array of ordercomboDtos '+orderComboDtos)
-
-
-
+    
+    
+    
       this.createOrder(order)
-
+    
       // empty cart for next order
       this.clearCart()
     },
 
-    async createOrder(order: OrderDTO): Promise<any> {
+    async createOrder(order: OrderDTO): Promise < any > {
       try {
         const baseURL = 'https://localhost:8080/api/'
-        const response = await fetch((`${baseURL}order/add`), {
+            const response = await fetch((`${baseURL}order/add`), {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify(order)
         });
-        if (!response.ok) {
-          throw new Error(`Failed to add order! status: ${response.status}`);
-        }
-        const result = await response.json();
-        return result;
-      } catch (error) {
-        console.error("Failed to create order:", error);
-        throw error;
-      }
+        if(!response.ok) {
+      throw new Error(`Failed to add order! status: ${response.status}`);
     }
-
-
+    const result = await response.json();
+    return result;
+          } catch (error) {
+      console.error("Failed to create order:", error);
+      throw error;
+    }
+        }
+    
   }
 })
