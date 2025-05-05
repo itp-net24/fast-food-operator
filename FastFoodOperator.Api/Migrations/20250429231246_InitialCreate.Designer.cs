48da13b7212490256798277c559642e76abab937
Jacob <jacobmannsclareus@gmail.com>
2025-05-05 10:29:37 +0200

    Resolv merge conflicts with develop

--- FastFoodOperator.Api/Migrations/20250429231246_InitialCreate.Designer.cs
+++ FastFoodOperator.Api/Migrations/20250429231246_InitialCreate.Designer.cs
@@ -4,16 +4,19 @@ using FastFoodOperator.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace FastFoodOperator.Api.Data.Migrations
+namespace FastFoodOperator.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250429231246_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
