736704e3fd2a3c4874221657987375b97491426d
Alexander <Aleexx.Hoang@gmail.com>
2025-05-06 19:45:51 +0200

    fix(orderFetcher): changed fetcher

--- FastFoodOperator.Client/src/utils/mappers.ts
+++ FastFoodOperator.Client/src/utils/mappers.ts
@@ -6,6 +6,7 @@ import type {
   Ingredient,
   Product,
   Variant,
+  Order,
 } from '@/models/types.ts'
 import { defaultVariantOfProduct, getTaxRate, isProductCombo } from '@/utils/helpers.ts'
 
@@ -142,6 +143,20 @@ export const mapToCartContainer = (base: BaseProduct, products: CartItem[]): Car
     price: base.basePrice,
     quantity: 1,
     products: products ?? [],
+  }
+}
+
+export const mapToOrder = (data:any): Order => {
+  return {
+    id: data.orderId,
+    orderNumber: data.orderNumber,
+    customerNote: data.customerNote,
+    orderStatus: data.orderStatus,
+    createdAt: data.createdAt,
+    startedAt: data.startedAt,
+    completedAt: data.completedAt,
+    orderProducts: data.orderProducts || [],
+    orderCombos: data.orderCombos || []
   }
 }
 
