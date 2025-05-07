fc8a1170c1408d9f84885b31a63383c1bd036d31
Jacob <jacobmannsclareus@gmail.com>
2025-05-07 17:07:24 +0200

    ReceiptComponent and TestReceipt added + minor change to DisplayOrderNumber

---
+++ FastFoodOperator.Client/src/views/TestReceipt.vue
@@ -0,0 +1,37 @@
+﻿<template>
+  <div>
+    <ReceiptComponent :order="mockOrder" @new-order="handleNewOrder" />
+  </div>
+</template>
+
+<script>
+import ReceiptComponent from '../components/ReceiptComponent.vue';
+
+export default {
+  components: {
+    ReceiptComponent
+  },
+  data() {
+    return {
+      mockOrder: {
+        products: [
+          { productName: 'Beer', productPrice: 40, quantity: 1 }
+        ],
+        combos: [
+          { comboName: 'Cheeseburger Combo', comboPrice: 25.99, quantity: 1 }
+        ],
+        createdAt: '2025-05-03 23:48',
+        orderNumber: 1027,
+        totalVatSixPercent: 0,
+        totalVatTwelvePercent: 2.78,
+        totalVatTwentyFivePercent: 8,
+        totalTax: 10.78,
+        orderFinalPrice: 65.99
+      }
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
