fa759fcd6a70c1c4aa04de0c7513e7cef9025fef
grahsp <pontus.grahsp@gmail.com>
2025-05-22 10:47:06 +0200

    refactor: move composable into a pinia store instead for shared state between kitchen and public display

--- FastFoodOperator.Client/src/stores/orderStore.ts
+++ FastFoodOperator.Client/src/stores/orderStore.ts
@@ -1,20 +1,31 @@
+import { defineStore } from 'pinia'
 import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr'
-import { GetOrders, DeleteOrder, StartOrder, CompleteOrder } from '@/services/fetcher.ts'
 import { ref } from 'vue'
+import { CompleteOrder, DeleteOrder, GetOrders, StartOrder } from '@/services/fetcher.ts'
 
-export default () => {
+export const orderStore = defineStore('', () => {
   const orders = ref([]);
 
-  const connection = new HubConnectionBuilder()
-    .withUrl("https://localhost:8080/test")
-    .configureLogging(LogLevel.Information)
-    .build();
-
-  const initializeAsync = async() => {
+  const init = async() => {
     orders.value = await GetOrders();
+
+    const connection = new HubConnectionBuilder()
+      .withUrl("https://localhost:8080/test")
+      .configureLogging(LogLevel.Information)
+      .build();
 
     connection.on("ReceiveOrder", (order) => {
       orders.value.push(order);
+    });
+
+    connection.on("CompleteOrder", (id: number, status: number) => {
+      const order = orders.value.find(o => o.orderId === id);
+      order.orderStatus = status;
+    });
+
+    connection.on("RemoveOrder", (id: number) => {
+      console.log("remove", id);
+      removeOrderById(id);
     });
 
     connection.start().catch(err => console.error(err.toString()));
@@ -22,8 +33,8 @@ export default () => {
 
   const setInProgressAsync = async(order) => {
     try {
-      await StartOrder({ orderId: order.orderId });
       order.orderStatus = 1;
+      await StartOrder({ orderId: order.orderId });
     } catch (error) {
       console.error('Fel vid uppdatering av order:', error);
     }
@@ -31,8 +42,8 @@ export default () => {
 
   const setCompleteAsync = async(order) => {
     try {
-      await CompleteOrder({ orderId: order.orderId }); // Pass orderId in an object
       order.orderStatus = 2;
+      await CompleteOrder({ orderId: order.orderId }); // Pass orderId in an object
     } catch (error) {
       console.error('Fel vid uppdatering av order:', error);
     }
@@ -43,7 +54,6 @@ export default () => {
       return;
 
     try {
-      console.log(order);
       await DeleteOrder(order.orderId);
       removeOrderById(order.orderId);
     } catch (error) {
@@ -53,16 +63,15 @@ export default () => {
 
   const removeOrderById = (id: number) => {
     const index = orders.value.findIndex(o => o.orderId === id);
+    if (index < 0) return;
     orders.value.splice(index, 1);
   }
 
   return {
     orders,
+    init,
     setInProgressAsync,
     setCompleteAsync,
     deleteOrderAsync,
-    initializeAsync,
   }
-}
-
-export type OrdersComposable = ReturnType<typeof useOrders>;
+});
